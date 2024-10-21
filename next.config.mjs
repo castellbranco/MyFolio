4e95628b67ae34950c9160ffc1253277dbfc39d0
Guilherme <guicbranco1@gmail.com>
2024-10-21 22:10:12 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  basePath: isProd ? '/MyFolio' : '', // Assuming your repo is called MyFolio
+  assetPrefix: isProd ? '/MyFolio/' : '',
   reactStrictMode: true,
   output: 'export',
+  images: {
+    unoptimized: true, // Because GitHub Pages doesn't support dynamic image optimization
+  },
 };
 
 export default nextConfig;
