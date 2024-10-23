5e7f1cf4d572ba6041194b9c47971bb0d12bb557
Guilherme <guicbranco1@gmail.com>
2024-10-23 15:48:12 +0100

    Corrigir erros em build

--- src/app/dist/dist/layout.dev.js
+++ src/app/dist/dist/layout.dev.js
@@ -13,7 +13,7 @@ var resume_1 = require("@/data/resume");
 
 var google_1 = require("next/font/google");
 
-require("./globals.css");
+require("@/app/globals.css");
 
 var fontSans = google_1.Inter({
   subsets: ["latin"],
