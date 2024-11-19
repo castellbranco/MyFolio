411fa2d9b42c9e04a89664771a711771fc959780
Guilherme <guicbranco1@gmail.com>
2024-11-19 12:00:59 +0000

    Vulnerabil changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export', // Export static HTML
   images: {
-    unoptimized: true, // Disable image optimization (GitHub Pages doesn't support it)
+    unoptimized: true,
   },
 };
 
